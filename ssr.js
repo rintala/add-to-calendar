5f0b857de2d007f2505f2c99d1e4bea80f55d719
rintala <jrintala@kth.se>
2021-01-31 20:03:54 +0100

    Update default - when creating element

--- ssr.js
+++ ssr.js
@@ -7,12 +7,15 @@ const ReactDOM = require("react-dom/server");
 // eslint-disable-next-line import/no-unresolved
 const { default: AddToCalendar } = require("./dist/AddToCalendar");
 
-ReactDOM.renderToString(React.createElement(AddToCalendar, {
-  event: {
-    name: "Happy Hour",
-    details: "Let's go after work",
-    location: "Boston, MA",
-    startsAt: "2018-12-06T17:00:00-05:00",
-    endsAt: "2018-12-06T18:00:00-05:00"
-  }
-}));
+ReactDOM.renderToString(
+  React.createElement(AddToCalendar, {
+    event: {
+      name: "Happy Hour",
+      details: "Let's go after work",
+      location: "Boston, MA",
+      url: "",
+      startsAt: "2018-12-06T17:00:00-05:00",
+      endsAt: "2018-12-06T18:00:00-05:00",
+    },
+  })
+);
